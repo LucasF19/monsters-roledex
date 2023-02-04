2b123ecc96b7224cffb9808378187cc43f47d785
Lucas Ferreira <lucas.maia@venhapranuvem.com.br>
2023-02-03 23:30:58 -0300

    Iniciando com map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      foods: [
+        {name: 'Macarrão'},
+        {name: 'Feijão'},
+        {name: 'Salada'},
+      ]
+    }    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.foods.map((food) => {
+            return <h1 key={food}>{food.name}</h1>
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
